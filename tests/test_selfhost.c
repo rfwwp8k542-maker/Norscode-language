79c9adf618e0e28ff5a99f637f1c3c484e86bdd5
jansteinarsaetre <jan.s.saetre@icloud.com>
2026-04-01 23:19:35 +0200

    selfhost: support rest and resten_av modulo aliases

--- tests/test_selfhost.c
+++ tests/test_selfhost.c
@@ -1267,7 +1267,7 @@ char * selfhost__compiler__uttrykk_til_ops_og_verdier_med_miljo(nl_list_text* to
             tok = "*";
             tok_step = 1;
         }
-        else if (nl_streq(tok_raw, "mod") || nl_streq(tok_raw, "modulo")) {
+        else if (((nl_streq(tok_raw, "mod") || nl_streq(tok_raw, "modulo")) || nl_streq(tok_raw, "rest")) || nl_streq(tok_raw, "resten_av")) {
             tok = "%";
             tok_step = 1;
         }
@@ -2301,6 +2301,10 @@ int start() {
     nl_assert_eq_text(expr_norsk_mod, "0: PUSH 17\n1: PUSH 5\n2: MOD\n3: PRINT\n4: HALT\n");
     char * expr_norsk_modulo = selfhost__compiler__disasm_uttrykk("17 modulo 5");
     nl_assert_eq_text(expr_norsk_modulo, "0: PUSH 17\n1: PUSH 5\n2: MOD\n3: PRINT\n4: HALT\n");
+    char * expr_norsk_rest = selfhost__compiler__disasm_uttrykk("17 rest 5");
+    nl_assert_eq_text(expr_norsk_rest, "0: PUSH 17\n1: PUSH 5\n2: MOD\n3: PRINT\n4: HALT\n");
+    char * expr_norsk_resten_av = selfhost__compiler__disasm_uttrykk("17 resten_av 5");
+    nl_assert_eq_text(expr_norsk_resten_av, "0: PUSH 17\n1: PUSH 5\n2: MOD\n3: PRINT\n4: HALT\n");
     char * expr_norsk_ganget_med = selfhost__compiler__disasm_uttrykk("3 ganget med 4");
     nl_assert_eq_text(expr_norsk_ganget_med, "0: PUSH 3\n1: PUSH 4\n2: MUL\n3: PRINT\n4: HALT\n");
     char * expr_norsk_ganget_med_underscore = selfhost__compiler__disasm_uttrykk("3 ganget_med 4");
@@ -2572,6 +2576,10 @@ int start() {
     nl_assert_eq_text(script_norsk_trekk_fra_underscore, "0: PUSH 10\n1: PUSH 3\n2: SUB\n3: PRINT\n4: HALT\n");
     char * script_norsk_mod = selfhost__compiler__disasm_skript("la x=17;la y=5;returner x mod y");
     nl_assert_eq_text(script_norsk_mod, "0: PUSH 17\n1: PUSH 5\n2: MOD\n3: PRINT\n4: HALT\n");
+    char * script_norsk_rest = selfhost__compiler__disasm_skript("la x=17;la y=5;returner x rest y");
+    nl_assert_eq_text(script_norsk_rest, "0: PUSH 17\n1: PUSH 5\n2: MOD\n3: PRINT\n4: HALT\n");
+    char * script_norsk_resten_av = selfhost__compiler__disasm_skript("la x=17;la y=5;returner x resten_av y");
+    nl_assert_eq_text(script_norsk_resten_av, "0: PUSH 17\n1: PUSH 5\n2: MOD\n3: PRINT\n4: HALT\n");
     char * script_norsk_ganget_med = selfhost__compiler__disasm_skript("la x=3;la y=4;returner x ganget med y");
     nl_assert_eq_text(script_norsk_ganget_med, "0: PUSH 3\n1: PUSH 4\n2: MUL\n3: PRINT\n4: HALT\n");
     char * script_norsk_ganget_med_underscore = selfhost__compiler__disasm_skript("la x=3;la y=4;returner x ganget_med y");
