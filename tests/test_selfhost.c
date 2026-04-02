e98c6ed08c1fd082366ffd1c0749830cce331342
jansteinarsaetre <jan.s.saetre@icloud.com>
2026-04-02 12:52:29 +0200

    selfhost: add public/private boolean aliases

--- tests/test_selfhost.c
+++ tests/test_selfhost.c
@@ -653,6 +653,9 @@ char * selfhost__compiler__normaliser_norsk_token(char * tok) {
     if (nl_streq(tok, "open")) {
         return "sann";
     }
+    if (nl_streq(tok, "public")) {
+        return "sann";
+    }
     if (nl_streq(tok, "enabled")) {
         return "sann";
     }
@@ -723,6 +726,9 @@ char * selfhost__compiler__normaliser_norsk_token(char * tok) {
         return "usann";
     }
     if (nl_streq(tok, "closed")) {
+        return "usann";
+    }
+    if (nl_streq(tok, "private")) {
         return "usann";
     }
     if (nl_streq(tok, "disabled")) {
@@ -3806,6 +3812,8 @@ int start() {
     nl_assert_eq_text(expr_english_trusted_untrusted_alias, "0: PUSH 1\n1: PUSH 0\n2: NOT\n3: AND\n4: PRINT\n5: HALT\n");
     char * expr_english_open_closed_alias = selfhost__compiler__disasm_uttrykk("open and not closed");
     nl_assert_eq_text(expr_english_open_closed_alias, "0: PUSH 1\n1: PUSH 0\n2: NOT\n3: AND\n4: PRINT\n5: HALT\n");
+    char * expr_english_public_private_alias = selfhost__compiler__disasm_uttrykk("public and not private");
+    nl_assert_eq_text(expr_english_public_private_alias, "0: PUSH 1\n1: PUSH 0\n2: NOT\n3: AND\n4: PRINT\n5: HALT\n");
     char * expr_english_equal_to_alias = selfhost__compiler__disasm_uttrykk("7 equal_to 7");
     nl_assert_eq_text(expr_english_equal_to_alias, "0: PUSH 7\n1: PUSH 7\n2: EQ\n3: PRINT\n4: HALT\n");
     char * expr_english_equal_to_phrase = selfhost__compiler__disasm_uttrykk("7 equal to 7");
@@ -4063,6 +4071,8 @@ int start() {
     nl_assert_eq_text(script_english_trusted_untrusted_alias, "0: PUSH 1\n1: PUSH 0\n2: NOT\n3: AND\n4: PRINT\n5: HALT\n");
     char * script_english_open_closed_alias = selfhost__compiler__disasm_skript("let x=open;return x and not closed");
     nl_assert_eq_text(script_english_open_closed_alias, "0: PUSH 1\n1: PUSH 0\n2: NOT\n3: AND\n4: PRINT\n5: HALT\n");
+    char * script_english_public_private_alias = selfhost__compiler__disasm_skript("let x=public;return x and not private");
+    nl_assert_eq_text(script_english_public_private_alias, "0: PUSH 1\n1: PUSH 0\n2: NOT\n3: AND\n4: PRINT\n5: HALT\n");
     char * script_english_math_alias = selfhost__compiler__disasm_skript("let x=8;let y=2;return x divided_by y");
     nl_assert_eq_text(script_english_math_alias, "0: PUSH 8\n1: PUSH 2\n2: DIV\n3: PRINT\n4: HALT\n");
     char * script_english_divided_by_phrase = selfhost__compiler__disasm_skript("let x=8;let y=2;return x divided by y");
