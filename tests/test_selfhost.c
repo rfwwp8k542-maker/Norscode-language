cc4c1de8ecf9265831130e4b21f9ce8540c0c2ff
jansteinarsaetre <jan.s.saetre@icloud.com>
2026-04-02 13:05:46 +0200

    selfhost: add correct/incorrect boolean aliases

--- tests/test_selfhost.c
+++ tests/test_selfhost.c
@@ -680,6 +680,9 @@ char * selfhost__compiler__normaliser_norsk_token(char * tok) {
     if (nl_streq(tok, "stable")) {
         return "sann";
     }
+    if (nl_streq(tok, "correct")) {
+        return "sann";
+    }
     if (nl_streq(tok, "enabled")) {
         return "sann";
     }
@@ -777,6 +780,9 @@ char * selfhost__compiler__normaliser_norsk_token(char * tok) {
         return "usann";
     }
     if (nl_streq(tok, "unstable")) {
+        return "usann";
+    }
+    if (nl_streq(tok, "incorrect")) {
         return "usann";
     }
     if (nl_streq(tok, "disabled")) {
@@ -3878,6 +3884,8 @@ int start() {
     nl_assert_eq_text(expr_english_working_broken_alias, "0: PUSH 1\n1: PUSH 0\n2: NOT\n3: AND\n4: PRINT\n5: HALT\n");
     char * expr_english_stable_unstable_alias = selfhost__compiler__disasm_uttrykk("stable and not unstable");
     nl_assert_eq_text(expr_english_stable_unstable_alias, "0: PUSH 1\n1: PUSH 0\n2: NOT\n3: AND\n4: PRINT\n5: HALT\n");
+    char * expr_english_correct_incorrect_alias = selfhost__compiler__disasm_uttrykk("correct and not incorrect");
+    nl_assert_eq_text(expr_english_correct_incorrect_alias, "0: PUSH 1\n1: PUSH 0\n2: NOT\n3: AND\n4: PRINT\n5: HALT\n");
     char * expr_english_equal_to_alias = selfhost__compiler__disasm_uttrykk("7 equal_to 7");
     nl_assert_eq_text(expr_english_equal_to_alias, "0: PUSH 7\n1: PUSH 7\n2: EQ\n3: PRINT\n4: HALT\n");
     char * expr_english_equal_to_phrase = selfhost__compiler__disasm_uttrykk("7 equal to 7");
@@ -4153,6 +4161,8 @@ int start() {
     nl_assert_eq_text(script_english_working_broken_alias, "0: PUSH 1\n1: PUSH 0\n2: NOT\n3: AND\n4: PRINT\n5: HALT\n");
     char * script_english_stable_unstable_alias = selfhost__compiler__disasm_skript("let x=stable;return x and not unstable");
     nl_assert_eq_text(script_english_stable_unstable_alias, "0: PUSH 1\n1: PUSH 0\n2: NOT\n3: AND\n4: PRINT\n5: HALT\n");
+    char * script_english_correct_incorrect_alias = selfhost__compiler__disasm_skript("let x=correct;return x and not incorrect");
+    nl_assert_eq_text(script_english_correct_incorrect_alias, "0: PUSH 1\n1: PUSH 0\n2: NOT\n3: AND\n4: PRINT\n5: HALT\n");
     char * script_english_math_alias = selfhost__compiler__disasm_skript("let x=8;let y=2;return x divided_by y");
     nl_assert_eq_text(script_english_math_alias, "0: PUSH 8\n1: PUSH 2\n2: DIV\n3: PRINT\n4: HALT\n");
     char * script_english_divided_by_phrase = selfhost__compiler__disasm_skript("let x=8;let y=2;return x divided by y");
